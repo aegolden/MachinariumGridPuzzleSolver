742fb062e4722ccb2ff6e52afbb261e20ae0c2bc
Aaron Golden <aegolden@gmail.com>
2015-07-31 17:46:08 -0700

    Initial Commit

---
+++ MachinariumGridSolver/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MachinariumGridSolver
+//
+//  Created by Aaron Golden on 7/31/15.
+//  Copyright (c) 2015 Aaron Golden. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ MachinariumGridSolver/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  MachinariumGridSolver
+//
+//  Created by Aaron Golden on 7/31/15.
+//  Copyright (c) 2015 Aaron Golden. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
